265db7980fa4d8e30ae5e22cfa841263b5cad11d
CPrince20 <rigbyrulles1@gmail.com>
2020-07-18 14:18:59 -0700

    Added weather_key into config

--- JS/config.js
+++ JS/config.js
@@ -1,3 +1,4 @@
 // API key
 const API_KEY = "pk.eyJ1IjoidGh1bmRyYWtrb24iLCJhIjoiY2tjMW5rOTlvMWRhOTJ5cGpubGhydm1weCJ9.kzJI_hnPomkYFrB97JX1Hw";
 const g_key = "AIzaSyBEcAJGaYe9ZKky_0h-8TGi7EBrWobW3T4"
+const weather_key = "API KEY HERE"

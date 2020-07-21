2d7846f6f23502532b539766c267152e60705cca
thundrakkon <DuongPTNguyen@gmail.com>
2020-07-21 07:48:50 -0700

    map.js click activated

--- JS/logic.js
+++ JS/logic.js
@@ -1,6 +1,5 @@
 // var apiKey = API_KEY;
 
-// var url = "http://127.0.0.1:5000";
 var url = "./Resources/city_join.json"
 // var proxy_url = "https://cors-anywhere.herokuapp.com/";
 // var heroku_url = "https://duong-homies.herokuapp.com/";
@@ -21,38 +20,6 @@ d3.json(url).then((jsonData) => {
     .classed("cities", true)
     .text(function(i){return i;})
     .exit().remove()
-  
-  // var city_values =[]
-  // for(var i=0; i < city.length; i++){
-  //   city_values += city[i]
-  // }
-  // console.log(city_values)
-  // var gapi_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + {city} + "," + {state} + "&key=" + {g_key}
-  // console.log(gapi_url)
-  // var myMap = L.map("map-id", {
-  //   center: [39.833333, -98.583333],
-  //   zoom: 4
-  // });
-
-  // L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  //   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  //   tileSize: 512,
-  //   maxZoom: 18,
-  //   zoomOffset: -1,
-  //   id: "mapbox/streets-v11",
-  //   accessToken: API_KEY
-  // }).addTo(myMap);
-
-  // var lat = jsonData.map(row => row.lat)
-  // var long = jsonData.map(row => row.long)
-
-  // var location = []
-  // for(var i = 0; i < city.length; i++){
-  //     if(city[i]){
-  //         location = lat[i]+","+long[i]
-  //     }
-  // }
-  // console.log(location)
 
 })
 
@@ -77,38 +44,6 @@ d3.json(url).then((jsonData) => {
 //   // var dropdownMenu = d3.select("#selDataset")
 //   // var dataset = dropdownMenu.property("value")
 //   // d3.event.preventDefault();
-
-//   var city = jsonData.map(row => row.city)
-//   var lat = jsonData.map(row => row.lat)
-//   var long = jsonData.map(row => row.long)
-
-//   var location = []
-//   for(var i = 0; i < city.length; i++){
-//       if(city[i]){
-//           location = lat[i]+","+long[i]
-//       }
-//   }
-//   console.log(location)
-// })
-
-// d3.json(url, function(response){
-//   console.log(response)
-//   // Create a new marker cluster group
-//   var markers = L.markerClusterGroup()
-//   // Loop through data
-//   for (var i=0; i<response.length; i++) {
-//     // Set the data location property to a variable
-//     var location = response[i].location
-//     // Check for location property
-//     if(location) {
-//       // Add a new marker to the cluster group and bind a pop-up
-//       markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]]))
-      
-//   // Add our marker cluster layer to the map
-//       .addTo(myMap);
-//     }
-//   }
-// })
 
 // d3.selectAll("#selDataset").on("change", optionChanged);
 
--- JS/map.js
+++ JS/map.js
@@ -1,10 +1,16 @@
+// Link to Json file
 var url = "./Resources/city_join.json"
+// var proxy_url = "https://cors-anywhere.herokuapp.com/";
+// var heroku_url = "https://duong-homies.herokuapp.com/";
+// var url = proxy_url + heroku_url
 
+// Creating map object
 var myMap = L.map("map-id", {
     center: [39.833333, -98.583333],
     zoom: 4
 });
 
+// Adding tile layer
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -14,40 +20,62 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: API_KEY
 }).addTo(myMap);
 
+// Grabbing Json data
 d3.json(url).then((jsonData) => {
+// d3.json(url, function(jsonData) {
+//     console.log(jsonData)
     var city = jsonData.map(row => row.city)
     var lat = jsonData.map(row => row.lat)
     var long = jsonData.map(row => row.long)
     var state = jsonData.map(row => row.state)
     var crime = jsonData.map(row => row.crime_index)
-    console.log(lat)
-    console.log(long)
+    var living = jsonData.map(row => row.cost_of_living_index)
 
+    // Adding pop-up marker for each city
     for (var d=0; d<city.length; d++) {
-        L.marker([lat[d], long[d]], {
+        var newMarker = L.marker([lat[d], long[d]], {
             fillOpacity: 0.75,
             color: "blue"
-        }).bindPopup(city[d] + ", " + state[d] + "<hr>" + "Crime Index: " + crime[d]).addTo(myMap)
+        })
+    
+    // Popup message
+    newMarker.bindPopup(city[d] + ", " + state[d] + "<hr>" + "Crime Index: " + crime[d] + "<br>" + "Cost of Living: " + living[d])
+    // When hovering mouse icon over the marker, display popup message
+    newMarker.on('mouseover', function (e) {
+        this.openPopup()
+    })
+    // When no longer hovering over the icon, close the popup
+    newMarker.on('mouseout', function (e) {
+        this.closePopup()
+    })
+    
+    // Add click to zoom and center functionality
+    newMarker.on('click', function (clickZoom){
+        myMap.setView(clickZoom.target.getLatLng(),12);
+    })
+
+    // Add to map
+    newMarker.addTo(myMap)
     }
+
+
 });
 
-// var data = []
+d3.selectAll("#selDataset").on("change", optionChanged)
 
-// d3.json(url, function(response){
-//     console.log(response[0])
-//     // Create a new marker cluster group
-//     var markers = L.markerClusterGroup()
-//     // Loop through data
-//     for (var i=0; i<response.length; i++) {
-//       // Set the data location property to a variable
-//       var location = response[i].location
-//       // Check for location property
-//       if(location) {
-//         // Add a new marker to the cluster group and bind a pop-up
-//         markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]]))
-        
-//     // Add our marker cluster layer to the map
-//         .addTo(myMap);
-//       }
-//     }
-// })
+function optionChanged() {
+    d3.json(url).then((jsonData) => {
+        var dropdownMenu = d3.select("#selDataset")
+        var dataset = dropdownMenu.property("value")
+
+        var city = jsonData.map(row => row.city)
+        var lat = jsonData.map(row => row.lat)
+        var long = jsonData.map(row => row.long)
+
+        for(var i = 0; i < city.length; i++) {
+            if(dataset == city[i]) {
+                myMap.setView(new L.LatLng(lat[i], long[i]), 12)
+            }
+        }
+    })
+}
